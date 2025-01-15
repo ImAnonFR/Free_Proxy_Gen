a81af5ab39e78edc9ef2dece135b8f31b20bd000
ImAnon <57401453+ImAnonFR@users.noreply.github.com>
2025-01-15 01:37:30 +0100

    Update get_proxy.py

--- get_proxy.py
+++ get_proxy.py
@@ -1,17 +1,12 @@
 import requests
-from time import sleep
-
 
 if __name__ == "__main__":
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0',
     }
     content = requests.get("https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all", headers=headers)
-    sleep(3)
+    print('Successfully download in http.txt!')
     result = content.text
-    result = result.replace("\n", "")
     text_file = open("http.txt", "w")
     text_file.write(result)
     text_file.close()
-    
-
